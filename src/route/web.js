ba5e52528fd1907c70911ffb8e8c63a2a7e8d570
Lee Ngoc <lee.ngoc6462@gmail.com>
2024-05-06 22:02:56 +0700

    Connect to SQL with Sequelize: Render data to view

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,18 @@
 import express from "express";
+import homeController from "../controllers/homeController";
+
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-	router.get("/", (req, res) => {
-        return res.send("Hello Lee Ngoc");
+	router.get("/", homeController.getHomePage);
+	router.get("/about", homeController.getAboutPage);
+
+
+	router.get("/leengoc", (req, res) => {
+        return res.send("Hello Lee Ngoc in undergroun");
     });
+
 	return app.use("/", router);
 }
 
